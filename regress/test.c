10bd5fa5484b3b2c6e32cd06a0c08a39f1a39e56
Mark Bucciarelli <mkbucc@gmail.com>
2014-08-04 20:44:37 -0400

    Null pointers should not crash (and they do).

--- regress/test.c
+++ regress/test.c
@@ -68,6 +68,29 @@ gt_quote_amp_lt_nospace()
 }
 
 
+void
+handle_null_src()
+{
+	char		*result = 0;
+
+	lives_ok( { htmlescape(0, result); }, "passing null src string should work." );
+}
+
+void
+handle_null_dst()
+{
+	lives_ok( { htmlescape("", 0); }, "passing null dst pointer should work." );
+}
+
+
+void
+handle_null_both()
+{
+	lives_ok( { htmlescape(0, 0); }, "both args null should work" );
+}
+
+
+
 
 int
 main (int argc, char *argv[])
@@ -81,6 +104,11 @@ main (int argc, char *argv[])
 	quote();
 	amp_lt_gt_quote();
 	gt_quote_amp_lt_nospace();
+
+	handle_null_src();
+	handle_null_dst();
+	handle_null_both();
+
 
 	done_testing();
 }
